89186e9dc115143b89e312e7791f8ff03c943457
Charisma Fadzri Triprakoso <fadzri@alterra.id>
2020-02-04 14:45:59 +0700

    penambahan peramban sweet alert

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import createStore from 'unistore';
 import axios from 'axios';
+import swal from 'sweetalert';
 
-// inisiasi variabel store
+// inisiasi variabel store yang akan digunakan
 const initialState = {
     logoKota: "https://jambikota.go.id/new/wp-content/uploads/Logojmb_edited2.jpg",
     namaKota: "Jambi",
@@ -14,34 +15,33 @@ const initialState = {
 
 export const store = createStore(initialState);
 
-export const actions = (store) => ({
+export const actions = store => ({
     // fungsi untuk mengarahkan formulir masuk
-    penangananMasuk : () => {
-        const self = this;
+    penangananMasuk: state => {
         const req = {
         method: 'post',
-        url: 'https://api.lokesal.online/masuk',
+        url: 'https://api.lokesal.online/admin/masuk',
         headers: {
             'Content-Type': 'application/json'
         },
         data: {
-            email: self.props.email,
-            kata_sandi: self.props.kataSandi
+            email: state.email,
+            kata_sandi: state.kataSandi
         }
         };
         axios(req)
         .then(function (response) {
             if (response.data.hasOwnProperty('token')) {
-            localStorage.setItem('email', self.props.username);
-            localStorage.setItem('adminId', self.props.username);
+            localStorage.setItem('email', state.email);
+            localStorage.setItem('adminId', state.adminId);
             localStorage.setItem('token', response.data.token);
             localStorage.setItem('sedangMasuk', true);
-            self.props.history.push('/');
+            this.props.history.push('/');
             }
             console.log('response data', response.data);
         })
         .catch(function (error) {
-            alert('email atau kata sandi tidak valid');
+            swal("Admin gagal masuk!", "Email atau kata sandi anda tidak valid", "error");
         });
     },
 
