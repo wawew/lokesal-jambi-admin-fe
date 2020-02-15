96015dede0416df9dd3e78f33383c08ee75231bc
Wildan Firdaus <firdaus@alterra.id>
2020-02-16 00:38:46 +0700

    mengganti profile docker

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd /var/www/lokesal-jambi-admin-fe
 git checkout release
 git pull
 
-source ~/.docker-profile
+source ~/.docker-profile-jambi-admin
 echo $DOCKERHUB_PASS | docker login --username $DOCKERHUB_USER --password-stdin
 docker pull wiflash/lokesal:jambi-admin-fe-latest
 docker stop lokesal-jambi-admin-frontend
