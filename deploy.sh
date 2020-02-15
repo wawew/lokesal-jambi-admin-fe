ccca974a5ac7f710413287a97cb3898da112086d
Wildan Firdaus <firdaus@alterra.id>
2020-02-16 00:41:07 +0700

    menghapus pull dari dockerhub

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ git pull
 
 source ~/.docker-profile-jambi-admin
 echo $DOCKERHUB_PASS | docker login --username $DOCKERHUB_USER --password-stdin
-docker pull wiflash/lokesal:jambi-admin-fe-latest
 docker stop lokesal-jambi-admin-frontend
 docker rm lokesal-jambi-admin-frontend
 docker rmi wiflash/lokesal:jambi-admin-fe-latest
