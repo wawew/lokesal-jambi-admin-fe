1ce791d346b6370b14f7d75652819ff050dc54ea
Wildan Firdaus <firdaus@alterra.id>
2020-02-16 02:06:51 +0700

    perbaikan pada dockerfile

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ git pull
 source ~/.docker-profile-jambi-admin
 echo $DOCKERHUB_PASS | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop lokesal-jambi-admin-frontend
-docker rm lokesal-jambi-admin-frontend
+docker container prune -f
 docker rmi wiflash/lokesal:jambi-admin-fe-latest
+docker image prune -f
 docker run -d --name lokesal-jambi-admin-frontend -p 3002:3002 wiflash/lokesal:jambi-admin-fe-latest
