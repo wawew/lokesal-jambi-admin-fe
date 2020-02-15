c40a8a6acae695c5365fcbc2c03931020fa036a3
Wildan Firdaus <firdaus@alterra.id>
2020-02-16 01:49:34 +0700

    ganti port

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ echo $DOCKERHUB_PASS | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop lokesal-jambi-admin-frontend
 docker rm lokesal-jambi-admin-frontend
 docker rmi wiflash/lokesal:jambi-admin-fe-latest
-docker run -d --name lokesal-jambi-admin-frontend -p 3000:3000 wiflash/lokesal:jambi-admin-fe-latest
+docker run -d --name lokesal-jambi-admin-frontend -p 3002:3002 wiflash/lokesal:jambi-admin-fe-latest
